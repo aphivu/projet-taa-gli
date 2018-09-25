9f1bcd6a6e4c3123f5e31ea4c2400fec9b325857
phi <alexandre.phi.vu@gmail.com>
2018-09-25 14:34:21 +0200

    Add sportController methodes

--- src/main/java/com/example/controller/SportController.java
+++ src/main/java/com/example/controller/SportController.java
@@ -1,11 +1,15 @@
 package com.example.controller;
 
+import com.example.dto.SportDTO;
+import com.example.entity.Sport;
 import com.example.repository.SportRepository;
 import com.example.service.ISportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Controller to handle all sport request
@@ -24,6 +28,24 @@ public class SportController {
 
     @Autowired
     ISportService iSportService;
+
+    @GetMapping("/all")
+    public @ResponseBody List<SportDTO> getAll(){
+        List<SportDTO> list = new ArrayList<SportDTO>();
+        Iterable<Sport> iterable = sportRepository.findAll();
+        for(Sport s:iterable){
+            list.add(iSportService.mapToDto(s));
+        }
+
+        return list;
+    }
+
+    @GetMapping("/{id}")
+    public @ResponseBody Sport getSingle(@PathVariable long id){
+        return sportRepository.getOne(id);
+    }
+
+
 
 
 }
